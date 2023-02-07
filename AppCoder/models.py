67a9068f176ea258bb5871c38215327508311977
Juan Rossi <jfrossi10@gmail.com>
2023-02-07 02:56:28 -0300

    Version 1

--- AppCoder/models.py
+++ AppCoder/models.py
@@ -2,19 +2,28 @@ from django.db import models
 
 # Create your models here.
 
+#Clase Modelo
 
-class Curso(models.Model):
+class Curso(models.Model):    
 
     grado = models.IntegerField()
+
+#Clase Estudiante
 
 class Estudiante(models.Model):
 
     nombre = models.CharField(max_length=30)
     apellido = models.CharField(max_length=30)
 
+#Clase Profesor
+
 class Profesor(models.Model):
 
     nombre = models.CharField(max_length=30)
     materia = models.CharField(max_length=20)
+
+
+
+
 
 
